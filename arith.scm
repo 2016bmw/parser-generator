8d5a70a7663cb3daab4d03cd10d06fbe2d9a5c43
Aaron L. Zeng <a2z@mit.edu>
2017-05-01 18:42:57 -0400

    Add arithmetic grammar example

---
+++ arith.scm
@@ -0,0 +1,40 @@
+(load "primitives")
+
+;;; Arithmetic Grammar Example
+
+(define p:digit
+  (p:choice
+   (p:string "0")
+   (p:string "1")
+   (p:string "2")
+   (p:string "3")
+   (p:string "4")
+   (p:string "5")
+   (p:string "6")
+   (p:string "7")
+   (p:string "8")
+   (p:string "9")))
+
+(define arithmetic-grammar
+  (letrec* ((number-rule (p:rule 'number (p:repeat p:digit 1 #f)))
+            (addend-rule
+             (p:choice number-rule
+                       (p:seq (p:string "(")
+                              (p:delayed (delay expr-rule))
+                              (p:string ")"))))
+            (add-rule
+             (p:rule 'sum
+                     (p:seq addend-rule
+                            (p:repeat (p:seq (p:string "+") addend-rule)
+                                      0 #f))))
+            (expr-rule (p:rule 'expr add-rule)))
+    expr-rule))
+
+;; Tests
+
+#|
+(arithmetic-grammar "1+2" try-match)
+;; ("parse-tree" (expr sum (number ("1")) ()))
+;; ("parse-tree" (expr sum (number ("1")) ((("+") (number ("2"))))))
+;; ;Value: #f
+|#
