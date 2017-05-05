d9fe282ccf7e711c1c92fa385fc29a884c074604
Varot Premtoon <varot@mit.edu>
2017-05-05 16:49:46 -0400

    Add simplify and tests for simplify

--- expression-example.scm
+++ expression-example.scm
@@ -29,11 +29,9 @@
 ;;; Grammar Rules
 
 #|
-
 atom     ::= number | var | '(' sum ')'
 product  ::= atom ('*' atom)*
 sum      ::= product ('*' product)*
-
 |#
 
 (define number-rule
@@ -130,5 +128,102 @@ sum      ::= product ('*' product)*
 ;; Value 41: (sum (product (number . 1) (number . 2) (number
 ;; . 2) (sum (product (number . 3)) (product (number
 ;; . 4)) (product (number . 5)))) (product (var . x)))
+
+|#
+
+(define (expr-tree-rule expr-tree) (car expr-tree))
+(define (expr-tree-value expr-tree) (cdr expr-tree))
+
+(define (number-node? expr-tree)
+  (eq? 'number (expr-tree-rule expr-tree)))
+(define (var-node? expr-tree)
+  (eq? 'var (expr-tree-rule expr-tree)))
+(define (product-node? expr-tree)
+  (eq? 'product (expr-tree-rule expr-tree)))
+(define (sum-node? expr-tree)
+  (eq? 'sum (expr-tree-rule expr-tree)))
+
+(define (simplify-number expr-tree env) expr-tree)
+
+(define (simplify-var expr-tree env)
+  (let* ((var (expr-tree-value expr-tree))
+	 (env-entry (assq var env)))
+    (if (pair? env-entry)
+	(cons 'number (cadr env-entry))
+	expr-tree)))
+
+(define (simplify-sum expr-tree env)
+  (let ((simplified-parts
+	 (map (lambda (expr) (simplify expr env))
+	      (expr-tree-value expr-tree))))
+    (if (every number-node? simplified-parts)
+	(cons 'number
+	      (apply + (map expr-tree-value simplified-parts)))
+	;; If there's just one argument, bring it out of sum
+	(if (= 1 (length simplified-parts))
+	    (car simplified-parts)
+	    (cons 'sum simplified-parts)))))
+    
+(define (simplify-product expr-tree env)
+  (let ((simplified-parts
+	 (map (lambda (expr) (simplify expr env))
+	      (expr-tree-value expr-tree))))
+    (if (every number-node? simplified-parts)
+	(cons 'number
+	      (apply * (map expr-tree-value simplified-parts)))
+	;; If there's just one argument, bring it out of product
+	(if (= 1 (length simplified-parts))
+	    (car simplified-parts)
+	    (cons 'product simplified-parts)))))
+
+(define (simplify expr-tree env)
+  (pp (list "simplifying" expr-tree))
+  (cond ((number-node? expr-tree)
+	 (simplify-number expr-tree env))
+	((var-node? expr-tree)
+	 (simplify-var expr-tree env))
+	((product-node? expr-tree)
+	 (simplify-product expr-tree env))
+	((sum-node? expr-tree)
+	 (simplify-sum expr-tree env))))
+
+#|
+
+;;; Test simplify
+
+(simplify (parse-expression "1") '())
+;Value 48: (number . 1)
+
+(simplify (parse-expression "x") '((x 4)))
+;Value 49: (number . 4)
+
+(simplify (parse-expression "y") '((x 4)))
+;Value 50: (var . y)
+
+(simplify (parse-expression "1 + 2") '((x 4)))
+;Value 57: (number . 3)
+
+(simplify (parse-expression "2 * 3") '((x 4)))
+;Value 58: (number . 6)
+
+(simplify (parse-expression "1 + 2 * 3 + 4") '((x 4)))
+;Value 59: (number . 11)
+
+(simplify (parse-expression "1 + x * 3 + 4") '((x 4)))
+;Value 62: (number . 17)
+
+(simplify (parse-expression "1 + 2 * 3 + x") '((x 4)))
+;Value 59: (number . 11)
+
+(simplify (parse-expression "1 + x*3 + y") '((x 4)))
+;Value 65: (sum (number . 1) (number . 12) (var . y))
+
+(simplify (parse-expression "1 + x*3*(y + 3*(1 + x)) + 4*(((z)))")
+	  '((x 4) (y 5) (z 6)))
+;Value 70: (number . 265)
+
+(simplify (parse-expression "1 + x*3*(y + 3*(1 + x)) + 4*(((z)))")
+	  '((x 4) (z 6)))
+;Value 71: (sum (number . 1) (product (number . 4) (number . 3) (sum (var . y) (number . 15))) (number . 24))
 
 |#
