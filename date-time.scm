7bb5bdce1a913f914762a6482299d01687e0b572
Bryan Williams <2016bmw@mit.edu>
2017-05-15 13:00:21 -0400

    Updated date-time example to work with new parse tree data structure

--- date-time.scm
+++ date-time.scm
@@ -17,16 +17,18 @@
     (else (error "invalid month" month))))
 
 (define (valid-date? parse-tree)
-  (let ((year (car parse-tree))
-        (month (cadr parse-tree))
-        (day (caddr parse-tree)))
+  (let* ((data (parse-tree-data parse-tree))
+	 (year (car data))
+	 (month (cadr data))
+	 (day (caddr data)))
     (and (<= 1 month 12)
          (<= 1 day (days-in-month month year)))))
 
 (define (valid-time? parse-tree)
-  (let ((hour (car parse-tree))
-        (minute (cadr parse-tree))
-        (second (caddr parse-tree)))
+  (let* ((data (parse-tree-data parse-tree))
+	(hour (car data))
+        (minute (cadr data))
+        (second (caddr data)))
     (and (<= 0 hour 23)
          (<= 0 minute 59)
          (<= 0 second 59))))
@@ -36,8 +38,7 @@
   (letrec* ((digits (lambda (digits)
                       (p:transform
                        (lambda (parse-tree)
-                         (string->number
-                          (apply string-append (map car (parse-tree-data parse-tree)))))
+                         (string->number (parse-tree-text parse-tree)))
                        (p:repeat (p:char-predicate char-numeric?) digits digits))))
             (date (p:rule 'date
                           (p:require valid-date?
@@ -57,30 +58,30 @@
 ;; UNIX Epoch
 (date-time-grammar "1970-01-01T00:00:00"
                    (lambda (date-time num-consumed)
-                     date-time))
+                     (parse-tree-data date-time)))
 ;; ;Value: (date-time (date 1970 1 1) (time 0 0 0))
 
 ;; Invalid month
 (date-time-grammar "2001-13-01T00:00:00"
                    (lambda (date-time num-consumed)
-                     date-time))
+                     (parse-tree-data date-time)))
 ;; ;Value: #f
 
 
 ;; Invalid day
 (date-time-grammar "2001-02-30T00:00:00"
                    (lambda (date-time num-consumed)
-                     date-time))
+                     (parse-tree-data date-time)))
 ;; ;Value: #f
 
 ;; Leap year
 (date-time-grammar "2004-02-29T00:00:00"
                    (lambda (date-time num-consumed)
-                     date-time))
+                     (parse-tree-data date-time)))
 ;; ;Value: (date-time (date 2004 2 29) (time 0 0 0))
 
 ;; Invalid times
 (date-time-grammar "2000-01-01T15:14:70"
                    (lambda (date-time num-consumed)
-                     date-time))
+                     (prase-tree-data date-time)))
 ;; ;Value: #f
